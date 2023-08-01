151eb00dfb78f7627d12331d01e4a0492fbd50c9
issamnasser <133253756+issamnasser@users.noreply.github.com>
2023-08-01 20:33:30 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint -  function that prints elements of a linked list
+ * @h: pointer to first node
+ * 
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		k++;
+		h = h->next;
+	}
+
+	return (k);
+}
