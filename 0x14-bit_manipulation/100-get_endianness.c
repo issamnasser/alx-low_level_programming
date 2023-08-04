a0b6bc07c1d9df01b8a07f04cdd71dec9813d011
issamnasser <133253756+issamnasser@users.noreply.github.com>
2023-08-04 19:05:53 +0100

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - function that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void);
+{
+	unsigned long int number = 1;
+	return (*(char *)&number);
+}
