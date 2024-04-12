28bd4a60c4bb36cd57dc66cdc07cc3602f23424a
Issam <Issamnasser56@gmail.com>
2024-04-12 02:41:16 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	
+
 	ht = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	ht->size = size;
 	ht->array = (hash_node_t **) malloc(size * sizeof(hash_node_t *));
 
